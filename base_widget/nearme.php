c95ea6aa3ab2ce91d66fb1a9381b7ce94df4124e
Nicolas Papin <nmpapin@gmail.com>
2012-01-29 18:16:08 -0500

    Working on getting multiple pages working.

--- base_widget/nearme.php
+++ base_widget/nearme.php
@@ -1,18 +1,35 @@
 <?php
-function getCloseFriends($lat, $long) 
-{
+	include 'db_helper.php';
 	
-
-	$friends = array();
-	$friends[0] = array(
-		"id" => "0",
-		"prism" => "npapin3",
-		"name" => "Nicolas Papin",
-		"location" => "CULC",
-		"img" => "img/thumbnail.jpg",
-		"status" => "Working on MAS Project!"
-	);
+	global $_USER;
+	$prismid = $_USER['uid'];
+	$curr_user_row = getDBResultRecord("SELECT user_id FROM user_table WHERE prism_id = '$prismid'");
+	$user_id = $curr_user_row['user_id'];
+	//echo($prismid);
 	
-	echo json_encode($friends);
-}
+	function getCloseFriends($location)
+	{ 		
+		$friends = array();
+		$friends[0] = array(
+			"id" => "0",
+			"prism" => "npapin3",
+			"name" => "Nicolas Papin",
+			"location" => "CULC",
+			"img" => "img/thumbnail.jpg",
+			"status" => "Working on MAS Project!"
+		);
+		
+		echo json_encode($friends);
+	}
+	
+	function getCloseLocations($location)
+	{
+		$locations = array();
+		$locations[0] = array(
+			"id" => "0",
+			"location" => "CULC"
+		);
+		
+		echo json_encode($locations);
+	}
 ?>
