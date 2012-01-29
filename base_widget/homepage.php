af45de8986fb64031ff418821b4966ac3032d5cd
Surenkumar Nihalani <virtualsuren@gmail.com>
2012-01-29 15:42:31 -0500

    Starting on getCloseFriends

---
+++ base_widget/homepage.php
@@ -0,0 +1,9 @@
+<?php
+	include 'db_helper.php';
+	global $_USER;
+	$userid = $_USER['uid'];
+	function getNearest(){
+	
+	}
+
+?>
--- base_widget/nearme.php
+++ base_widget/nearme.php
@@ -1,18 +1,18 @@
 <?php
-function getCloseFriends($lat, $long) 
-{
-	
-
-	$friends = array();
-	$friends[0] = array(
-		"id" => "0",
-		"prism" => "npapin3",
-		"name" => "Nicolas Papin",
-		"location" => "CULC",
-		"img" => "img/thumbnail.jpg",
-		"status" => "Working on MAS Project!"
-	);
-	
-	echo json_encode($friends);
-}
+	include 'db_helper.php';
+	global $_USER;
+	$userid = $_USER['uid'];
+	function getCloseFriends($lat, $long){ 
+		$friends = array();
+		$friends[0] = array(
+			"id" => "0",
+			"prism" => "npapin3",
+			"name" => "Nicolas Papin",
+			"location" => "CULC",
+			"img" => "img/thumbnail.jpg",
+			"status" => "Working on MAS Project!"
+		);
+		
+		echo json_encode($friends);
+	}
 ?>
