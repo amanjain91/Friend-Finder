0650720b896d51a0fc473a986c1abb2e9c204f2e
Nicolas Papin <nmpapin@gmail.com>
2012-02-01 21:34:10 -0500

    Working friends list.

--- base_widget/friends.php
+++ base_widget/friends.php
@@ -9,7 +9,7 @@
 		$friend_ids = getFriends(getUserId());
 		
 		$qry_str = "'" . implode("','", $friend_ids) . "'";
-		$query = "SELECT prism_id AS username, first_name, last_name, img_url FROM user_table WHERE user_id IN ($qry_str);";
+		$query = "SELECT user_id AS id, first_name, last_name, img_url FROM user_table WHERE user_id IN ($qry_str);";
 		
 		$friends = getDBResultsArray($query);
 		
