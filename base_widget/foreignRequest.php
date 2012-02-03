e22d047994c1ecf28aa26145ec28558666514f06
Surenkumar Nihalani <virtualsuren@gmail.com>
2012-02-03 02:45:34 -0500

    Worked on getting API data into our data base.

--- base_widget/foreignRequest.php
+++ base_widget/foreignRequest.php
@@ -1,9 +1,34 @@
 <?php
 
-function getGTPlacesData()
-{
+function getGTPlacesData() {
 	$locs = json_decode(file_get_contents("http://m.cip.gatech.edu/api/gtplaces/buildings"), true);
-
+	$i = 0;
+	foreach($locs as $rows){
+		$i++;
+		$b_id = $rows["building_id"];
+		$lat = $rows["latitude"];
+		$lon = $rows["longitude"];
+		$name = $rows["name"];
+		$img = $rows["image_url"];
+		$sql  = "INSERT INTO location_table (
+					location_id, 
+					building_id, 
+					longitude, 
+					latitude, 
+					building_name, 
+					img_url
+				)
+				VALUES (
+					$i, 
+					$b_id, 
+					$lon, 
+					$lat,
+					$name, 
+					$img
+				)";
+		//I don't know what function from db helper to call.
+		// FIXME for Nick.
+	}
 	return $locs;
 }
 
