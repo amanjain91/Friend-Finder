f44fd1f686a7d72b7737febb33329d57839e3dca
Surenkumar Nihalani <virtualsuren@gmail.com>
2012-02-04 17:08:00 -0500

    Added commenting.

--- base_widget/foreignRequest.php
+++ base_widget/foreignRequest.php
@@ -1,14 +1,23 @@
 <?php
-
+/**
+* This PHP function gets the data from GT places, takes out address and puts it in our database.
+* run this to update our database.
+*/
 function getGTPlacesData() {
 	$locs = json_decode(file_get_contents("http://m.cip.gatech.edu/api/gtplaces/buildings"), true);
 	$i = 0;
 	foreach($locs as $rows){
+		//Incrementing location id
 		$i++;
+		//Building Id.
 		$b_id = $rows["building_id"];
+		//Latitude
 		$lat = $rows["latitude"];
+		//Longitude
 		$lon = $rows["longitude"];
+		//The building name.
 		$name = $rows["name"];
+		//The image URL of the page.
 		$img = $rows["image_url"];
 		$sql  = "INSERT INTO location_table (
 					location_id, 
@@ -18,6 +27,7 @@ function getGTPlacesData() {
 					building_name, 
 					img_url
 				)
+				
 				VALUES (
 					$i, 
 					$b_id, 
