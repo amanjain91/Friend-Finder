03f32ff9aa36a23df0bc72098a5bd495ac17955c
Nicolas Papin <nmpapin@gmail.com>
2012-02-04 17:52:02 -0500

    Finished populate script.

--- base_widget/foreignRequest.php
+++ base_widget/foreignRequest.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once 'db_helper.php';
+
 /**
 * This PHP function gets the data from GT places, takes out address and puts it in our database.
 * run this to update our database.
@@ -8,31 +11,17 @@ function getGTPlacesData() {
 	foreach($locs as $rows)
 	{
 		//Building Id.
-		$b_id = $rows["building_id"];
+		$b_id = mysql_real_escape_string($rows["b_id"]);
 		//Latitude
 		$lat = $rows["latitude"];
 		//Longitude
 		$lon = $rows["longitude"];
 		//The building name.
-		$name = $rows["name"];
+		$name = mysql_real_escape_string($rows["name"]);
 		//The image URL of the page.
-		$img = $rows["image_url"];
-		$sql  = "INSERT INTO location_table (
-					location_id, 
-					building_id, 
-					longitude, 
-					latitude, 
-					building_name, 
-					img_url
-				)
-				
-				VALUES (
-					'$b_id', 
-					'$lon', 
-					'$lat',
-					'$name', 
-					'$img'
-				)";
+		$img = mysql_real_escape_string($rows["image_url"]);
+		$sql  = "INSERT INTO location_table (building_id, longitude, latitude, building_name, img_url) 
+				VALUES ('$b_id', $lon, $lat, '$name', '$img');";
 		
 		getDBResultInserted($sql, 0);
 	}
