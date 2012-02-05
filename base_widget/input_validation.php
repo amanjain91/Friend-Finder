89f41688c6e01a4eaf287e1324fa8d969bef372b
Surenkumar Nihalani <virtualsuren@gmail.com>
2012-02-05 15:48:51 -0500

    First name, last name validator done.

---
+++ base_widget/input_validation.php
@@ -0,0 +1,29 @@
+<?php	
+	/**A function to check if the given email is valid or not**/
+	function is_valid_email($an_email){
+		if(filter_var($email_a, FILTER_VALIDATE_EMAIL)){
+			return true;
+		}
+		return false;
+	}
+	
+	/**checks if this phone number contains 10 digits.**/
+	function is_valid_phone_number($a_number){
+		$all_letters = str_split($a_number);
+		$count_of_valid_numbers = 0;
+		for($i = 0; $i < sizeof($all_letters); $i++){
+			if(ctype_digit($all_letters[$i])){
+				$count_of_valid_numbers++;
+			}
+		}
+		if($count_of_valid_numbers == 10){
+			return true;
+		}
+		return false;
+	}
+	
+	/**Checks if this is a vald last name or first name**/
+	function is_valid_name($a_name){
+		return ctype_alpha($a_name);
+	}
+?>
