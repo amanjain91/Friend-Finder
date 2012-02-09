cf637b91e27d943189217ed31bc9ee311ae789de
Surenkumar Nihalani <suren@gatech.edu>
2012-02-08 21:14:04 -0500

    Finished get pending requests

---
+++ FriendLoc/friend_request.php
@@ -0,0 +1,54 @@
+<?php
+	require_once 'common_functions.php';
+	
+	/** Gets the pending requests for the the currently logged in report. **/
+	function get_pending_friends(){
+		/** The return value **/
+		$my_array = array();
+		/** Getting the currently logged in user. **/
+		$my_user = getUserId();
+		$sql =	"
+			SELECT user_a
+			FROM friend_map
+			WHERE user_b = '$my_user'
+			AND direction = '0'
+			AND status = '0'
+				";
+		$results = getDBResultsArray($sql);
+		foreach($results as $row){
+			array_push($my_array, $row["user_a"]);
+		}
+		$sql = "
+			SELECT user_b
+			FROM friend_map
+			WHERE user_a = '$my_user'
+			AND direction = '1'
+			AND status = '0'
+				";
+		$results = getDBResultsArray($sql);
+		foreach($results as $row){
+			array_push($my_array, $row["user_b"]);
+		}
+		echo json_encode($my_array);
+	}
+	
+	/** Send request **/
+	function send_request($a_friend_id){
+		$le_user = getUserId();
+		$sql = "
+				INSERT INTO friend_map (
+					user_a,
+					user_b,
+					direction,
+					status)
+				VALUES(
+					'$le_user',
+					'$a_friend_id',
+					'0',
+					'0'
+				)
+		";
+		
+		
+	}
+?>
